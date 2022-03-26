5187cb5a85a2d00baf2581122d0580d445018251
Aditya <aditya.gupta671@gmail.com>
2022-03-27 00:37:07 +0530

    New Updates

--- whatsapp_clone/src/chat.js
+++ whatsapp_clone/src/chat.js
@@ -4,7 +4,7 @@ import { Avatar, IconButton } from '@mui/material';
 import { AttachFileOutlined, SearchOutlined, MoreVertOutlined, InsertEmoticon, MicOutlined, SendSharp } from '@mui/icons-material';
 import {useParams} from "react-router-dom";
 import { firestoreDb } from './firebase';
-import { onSnapshot, collection, doc, query, getDocs, serverTimestamp, addDoc,setDoc } from 'firebase/firestore';
+import { onSnapshot, collection, doc, query, serverTimestamp, addDoc,setDoc, orderBy } from 'firebase/firestore';
 import { useStateValue } from './contextStateProvider';
 import SpeechRecognition, {
    useSpeechRecognition
@@ -15,6 +15,7 @@ const Chat = (props) => {
    const [value,setValue] = useState("");
    const {roomId} = useParams();
    const [{user}] = useStateValue();
+   const [sender,setSender] = useState("");
    const [messages,setMessages] = useState("");
    const [speechActive,setSpeechActive] = useState(true);
    const { transcript, resetTranscript } = useSpeechRecognition();
@@ -24,12 +25,14 @@ const Chat = (props) => {
          onSnapshot(doc(firestoreDb, "whatsapp",roomId), (d) => {
             setRoomName(d.data().name);
          });
-         onSnapshot(collection(firestoreDb, `whatsapp/${roomId}/messages`), (d) => {
+         const qq = query(collection(firestoreDb, `whatsapp/${roomId}/messages`), orderBy("timestamp", "asc"));
+         onSnapshot(qq, (d) => {
             setMessages(d.docs.map(v => ({
                ...v.data()
             })
             ))
          });
+         console.log(messages)
       }
    },[roomId]);
    // useEffect(() => {
@@ -47,7 +50,7 @@ const Chat = (props) => {
       if(user){
          const name = user.displayName.split(" ")
          // setUserName(user.displayName);
-         // setSender(name[0].charAt(0).toUpperCase() + name[0].slice(1,name[0].length))
+         setSender(name[0].charAt(0).toUpperCase() + name[0].slice(1,name[0].length))
          // setRoomFace(user.photoURL);
       }
       
@@ -62,30 +65,20 @@ const Chat = (props) => {
          
       }
    }
-   const sendMessage = async (e) => {
-      e.preventDefault();
-      const q = query(collection(firestoreDb,"whatsapp"));
-      const querySnapshot = await getDocs(q);
+   const sendMessage = () => {
+      // const q = query(collection(firestoreDb,"whatsapp"));
+      // const querySnapshot = await getDocs(q);
       
       if(value !== ""){
-         const queryData = querySnapshot.docs.map((doc) => ({
-            ...doc.data(),
-            id:doc.id
-         }))
-         queryData.map(async (v,id) => {
-            if(v.id === roomId){
-               await setDoc(doc(firestoreDb, `whatsapp/${roomId}/messages`,doc.data()), {
-                  userName: user.displayName,
-                  message:value,
-                  userImage:user.photoURL,
-                  timestamp: serverTimestamp(),
-               });
-            }
-
-         })
-
+         setDoc(doc(collection(firestoreDb, `whatsapp/${roomId}/messages`)), {
+            userName: sender,
+            message:value,
+            userImage:user.photoURL,
+            timestamp: serverTimestamp(),
+         });
          resetTranscript();
       }
+      setValue("a");
    }
    const MicSpeechToText = (e) => {
       e.preventDefault();
@@ -120,18 +113,14 @@ const Chat = (props) => {
          </div>
          <div className='chat__body'>
             {messages ? 
-            messages.map((mess) => {
-            <Fragment>
-               <p className='chat__message chat__sender'>{mess.userName} 
-                  <span className='chat__name'>{mess.message}</span>
-                  <span className='chat__timestamp'>3:25pm</span>
+            messages.map((mess,index) => {
+            return (<Fragment key={index}>
+               <p className={`chat__message ${mess.userName === sender ? 'chat__sender' : 'chat__receiver'}`}>
+                  {mess.message}
+                  <span className='chat__name'>{mess.userName} </span>
+                  <span className='chat__timestamp'>{new Date(mess.timestamp?.toDate()).toLocaleTimeString()}</span>
                </p>
-               <p className='chat__message chat__receiver'>
-                  {mess.message} 
-                  <span className='chat__name'>{mess.userName}</span>
-                  <span className='chat__timestamp'>4:25pm</span>
-               </p>
-            </Fragment>         
+            </Fragment> )        
             }) : 
                <Fragment>
                   <p className='chat__message chat__sender'>message
@@ -152,17 +141,15 @@ const Chat = (props) => {
             </IconButton>
             <form className='chat__footerContainer'>
                <input type="text" placeholder="Type your message" name="ChatText"
-               onChange={e => setValue(e.target.value)}
+               onChange={(e) => setValue(e.target.value)}
                />
-               {/* <span>{value + transcript}</span> */}
-               <IconButton onClick={sendMessage}>
+               <IconButton  onClick={sendMessage} >
                   <SendSharp/>
                </IconButton>
-               <IconButton onClick={MicSpeechToText}>
+            </form>  
+            <IconButton onClick={MicSpeechToText}>
                   <MicOutlined/>
                </IconButton>
-            </form>  
-            
          </div>
       </div>
    );
